39d021e9090e63d499e970c7c4de71e4ccedb430
yjmjaiqi <81315676+yjmjaiqi@users.noreply.github.com>
2022-11-23 11:56:30 +0800

    Delete user.sql

--- user.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` VALUES (2, 'xxzx', '4QrcOUm6Wau+VuBX8g+IPg==', 'B200603', '区块链学院');
-INSERT INTO `user` VALUES (6, '佳期如梦', '4QrcOUm6Wau+VuBX8g+IPg==', 'B200104', '智能科技学院');
-INSERT INTO `user` VALUES (7, 'yjm', '4QrcOUm6Wau+VuBX8g+IPg==', 'B200603', '区块链学院');
-INSERT INTO `user` VALUES (8, 'zyf', '4QrcOUm6Wau+VuBX8g+IPg==', 'B200602', '区块链学院');
-INSERT INTO `user` VALUES (9, 'cjx', '4QrcOUm6Wau+VuBX8g+IPg==', 'B200603', '区块链学院');
